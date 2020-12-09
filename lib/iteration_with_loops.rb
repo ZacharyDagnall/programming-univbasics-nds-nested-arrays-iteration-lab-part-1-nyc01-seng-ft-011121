b93c72ddcfb1d108e6e6f9c63d0eade94c7bcbe6
Zachary R Dagnall <zachary.dagnall@gmail.com>
2020-12-09 15:59:52 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,22 +3,22 @@ require 'pry'
 def find_even_values(src)
   xcount=0 
   ycount=0 
-  while xcount < src.length do
-    while ycount < src[xcount].length do
-      n = src[xcount][ycount]
-      binding.pry
-      if n%2==0
-        puts n
-      end
-      ycount+=1
-    end 
-    xcount+=1 
-  end 
-  # for i in 0...src.length do 
-  #   for j in 0...src[i].length do
-  #     if (src[i][j]%2==0)
-  #       puts src[i][j]
+  # while xcount < src.length do
+  #   while ycount < src[xcount].length do
+  #     n = src[xcount][ycount]
+  #     binding.pry
+  #     if n%2==0
+  #       puts n
   #     end
-  #   end
-  # end
+  #     ycount+=1
+  #   end 
+  #   xcount+=1 
+  # end 
+  for i in 0...src.length do 
+    for j in 0...src[i].length do
+      if (src[i][j]%2==0)
+        puts src[i][j]
+      end
+    end
+  end
 end 
